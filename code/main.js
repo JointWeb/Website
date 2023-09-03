fd4e1652bef7e9eac9bcb69428eaea87d715b0c1
JointWeb <jtsalem@outlook.com>
2023-09-03 18:46:52 -0400

    Main Layout
    Setup up the Menu and Layout

---
+++ code/main.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector(".hamburger"); 
+const navMenu = document.querySelector(".nav-links");
+
+function makeActive () {
+     hamburger.classList.toggle("active");
+     navMenu.classList.toggle("active");
+}
+
+hamburger.addEventListener('click', makeActive);
+
+const main = document.querySelector("main");
+
+function removeActive () {
+     hamburger.classList.remove("active");
+     navMenu.classList.remove("active");
+}
+
+main.addEventListener('click', removeActive);
